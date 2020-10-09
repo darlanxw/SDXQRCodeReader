3b439afc76b48968968bd479e4d1cf99304a9cc5
Darlan Borges <darlanxw@gmail.com>
2020-10-09 11:31:03 -0300

    changing method names

--- www/SDXQRCodeReader.js
+++ www/SDXQRCodeReader.js
@@ -1,9 +1,7 @@
 var exec = require('cordova/exec');
-var SDXQRCodeReader = function () { }; // This just makes it easier for us to export all of the functions at once.
-// All of your plugin functions go below this. 
-// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-SDXQRCodeReader.coolMethod = function (arg0, onSuccess, onError) {
-    console.log("Chegou no plugin!")
-    exec(onSuccess, onError, "SDXQRCodeReader", "coolMethod", [arg0]);
+var SDXQRCodeReader = function () { }; 
+
+SDXQRCodeReader.scanQRCode = function (arg0, onSuccess, onError) {
+    exec(onSuccess, onError, "SDXQRCodeReader", "scanQRCode", [arg0]);
 };
 module.exports = SDXQRCodeReader;
