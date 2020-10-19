d2f2d0a65100867baea8214f6350a33becfa9f39
Darlan Borges <darlanxw@gmail.com>
2020-10-18 23:18:21 -0300

    adding support to android

--- src/android/SDXQRCodeReader.java
+++ src/android/SDXQRCodeReader.java
@@ -1,8 +1,11 @@
 package com.sdx.qrcodereader;
 
-import org.apache.cordova.CordovaPlugin;
+import android.widget.Toast;
+import com.sodexo.qrcodegenerator.SDXQRGenerator;
+import com.sodexo.qrcodegenerator.SDXQrCodeListener;
+import com.sodexo.qrcodegenerator.domain.SDXQRCodeTransaction;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,16 +15,56 @@ import org.json.JSONObject;
  */
 public class SDXQRCodeReader extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("scanQRCode")) {
-            this.scanQRCode(callbackContext);
-            return true;
-        }
-        return false;
+  @Override public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("scanQRCode")) {
+      this.scanQRCode(callbackContext);
+      return true;
     }
+    return false;
+  }
 
-    private void scanQRCode(CallbackContext callbackContext) {
-        callbackContext.error("It was not possible to parse the data.");
-    }
+  private void scanQRCode(CallbackContext callbackContext) {
+    new SDXQRGenerator().scan(cordova.getContext(), false, false, new SDXQrCodeListener() {
+      @Override public void success(SDXQRCodeTransaction model) {
+        try {
+          JSONObject json = new JSONObject();
+          json.put("merchantId", model.getMerchantId());
+          json.put("merchantUserId", model.getMerchantUserId());
+          json.put("city", model.getCity());
+          json.put("country", model.getCountry());
+          json.put("currency", model.getCurrency());
+          if (model.getDateTime() != null) {
+            json.put("dateTime", model.getDateTime());
+          }
+          if (model.getMerchantBranchCode() != null) {
+            json.put("merchantBranchCode", model.getMerchantBranchCode());
+          }
+          if (model.getTransactionId() != null) {
+            json.put("transactionId", model.getTransactionId());
+          }
+          json.put("dateTime", model.getDateTime());
+          if (model.getTip() != null) {
+            json.put("tip", model.getTip());
+          }
+          if (model.getTransactionAmount() != null) {
+            json.put("transactionAmount", model.getTransactionAmount());
+          }
+          cordova.getActivity().finish();
+          callbackContext.success(json);
+          cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override public void run() {
+              Toast.makeText(cordova.getContext(), json.toString(), Toast.LENGTH_SHORT).show();
+            }
+          });
+        } catch (JSONException e) {
+          callbackContext.error(e.getMessage());
+        }
+      }
+
+      @Override public void error() {
+        callbackContext.error("Error to handle");
+      }
+    });
+    callbackContext.error("It was not possible to parse the data.");
+  }
 }
