2dfc09562a860f3c4013fab9a8c606704564a04a
Darlan Borges <darlanxw@gmail.com>
2020-10-21 16:50:08 -0300

    added alert debug

--- www/SDXQRCodeReader.js
+++ www/SDXQRCodeReader.js
@@ -2,7 +2,9 @@ var exec = require('cordova/exec');
 var SDXQRCodeReader = function () { }; 
 
 SDXQRCodeReader.scanQRCode = function (onSuccess, onError) {
+    alert("Chegou no plgin JS");
     exec(function (param) {
+        alert("Voltou do plugin nativo!");
         var sdxTransaction = JSON.parse(param);
         return onSuccess(sdxTransaction)
     }, onError, "SDXQRCodeReader", "scanQRCode", [null]);
