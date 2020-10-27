65124622c544aa1f0b8c45c62626d88ca3c24cb9
Darlan Borges <darlanxw@gmail.com>
2020-10-27 14:15:20 -0300

    changing ios sdk version

---
+++ src/ios/SDXQRCodeGenerator.xcframework/ios-i386_x86_64-simulator/SDXQRCodeGenerator.framework/Headers/SDXQRCodeGenerator.h
@@ -0,0 +1,19 @@
+//
+//  SDXQRCodeGenerator.h
+//  SDXQRCodeGenerator
+//
+//  Created by Saulo Costa on 10/09/20.
+//  Copyright © 2020 Saulo Costa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDXQRCodeGenerator.
+FOUNDATION_EXPORT double SDXQRCodeGeneratorVersionNumber;
+
+//! Project version string for SDXQRCodeGenerator.
+FOUNDATION_EXPORT const unsigned char SDXQRCodeGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDXQRCodeGenerator/PublicHeader.h>
+
+
